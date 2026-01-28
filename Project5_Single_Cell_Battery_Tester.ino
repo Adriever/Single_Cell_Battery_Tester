fc97b9826cb40d6f08de541fc8b88503629e5c5b
Adriever <k0911351@student.kirkwood.edu>
2026-01-28 11:04:46 -0600

    Add files via upload

---
+++ Project5_Single_Cell_Battery_Tester.ino
@@ -0,0 +1,36 @@
+#define newLED 2
+#define okLED 4
+#define oldLED 6
+int analogValue = 0;
+float voltage = 0;
+int ledDelay = 2000;
+
+void setup()
+{
+pinMode(newLED, OUTPUT);
+pinMode(okLED, OUTPUT);
+pinMode(oldLED, OUTPUT);
+}
+void loop()
+{
+analogValue = analogRead(0);
+voltage = 0.0048*analogValue;
+if ( voltage >= 1.4 )
+{
+digitalWrite(newLED, HIGH);
+delay(ledDelay);
+digitalWrite(newLED, LOW);
+}
+else if ( voltage < 1.4 && voltage > 1.1 )
+{
+digitalWrite(okLED, HIGH);
+delay(ledDelay);
+digitalWrite(okLED, LOW);
+}
+else if ( voltage <= 1.0 )
+{
+digitalWrite(oldLED, HIGH);
+delay(ledDelay);
+digitalWrite(oldLED, LOW);
+}
+}
